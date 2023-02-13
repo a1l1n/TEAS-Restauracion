bacb06ee951de20bf018a17b8e97cc8a37a0ade5
Ailín <ailin.tobares@gmail.com>
2023-02-12 23:13:46 -0300

    Update 12/02/2023

--- src/Components/4 - Contact/Contact.jsx
+++ src/Components/4 - Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import Swal from 'sweetalert2';
 import Styles from "./Contact.module.css";
 
 export const Contact = () => {
@@ -34,52 +35,63 @@ export const Contact = () => {
   return (
     <section id="contact">
         <div className={Styles.contact_container}>
-            <div className={Styles.contact_image}></div>
 
             <div className={Styles.contact_form}>
                 <h1 className={Styles.contact_title}>Contactanos</h1>
 {/*                 <h3>O podés dejarnos tu mensaje aquí: </h3> */}
                 <form ref={form} onSubmit={sendEmail}>
                     <div className={Styles.contact_form_div}>
-                        <label>Nombre: </label>
+                        <div className={Styles.contact_form_label}>
+                            <label className={Styles.contact_form_label}>Nombre: </label>
+                        </div>
                         <input
                         type="text"
                         name="user_name"
-                        placeholder="Dejanos tu nombre"
+                        /* placeholder="Dejanos tu nombre" */
                         className={Styles.contact_form_input}
                         required
                         />
                     </div>
 
                     <div className={Styles.contact_form_div}>
-                        <label>Tu e-mail</label>
+                        <div className={Styles.contact_form_label}>
+                            <label>E-mail</label>
+                        </div>
                         <input
                         type="email"
                         name='user_email'
-                        placeholder="Dejanos tu e-mail"
+                        /* placeholder="Dejanos tu e-mail" */
                         className={Styles.contact_form_input}
                         required
                         />
                     </div>
 
                     <div className={Styles.contact_form_div}>
-                        <label>Asunto</label>
+                        <div className={Styles.contact_form_label}>
+                            <label className={Styles.contact_form_label}>Asunto</label>
+                        </div>
                         <input
                         type="text"
                         name="user_subject"
-                        placeholder="Dejanos el asunto"
+                        /* placeholder="Dejanos el asunto" */
                         className={Styles.contact_form_input}
                         required
                         />
                     </div>
 
                     <div className={Styles.contact_form_div}>
-                        <label>Y el mensaje</label>
+                        <div  className={Styles.contact_form_label}> 
+                            <label>Y el mensaje</label>
+                        </div>
+                       
                         <textarea
                         type="message"
-                        cols="30"
+                        name="message"
+                        cols="40"
                         rows="10"
                         placeholder="Compartinos tu consulta o proyecto"
+                        minlength="20"
+                        maxLength="300"
                         className={Styles.contact_form_textarea}
                         required
                         />
@@ -89,18 +101,13 @@ export const Contact = () => {
 
                 </form>
             </div>
-                {/* Nombre, mail, asunto, mensaje */}
+
+            <div className={Styles.contact_image}>
+                <div className={Styles.contact_image_description}>
+                    <h3>Limpieza de policromía - Recinto del Senado, Congreso de la Nación - Año 2006, Progorod S.A</h3>
+                </div>
+            </div>
         </div>
     </section>
   )
 }
-
-/* 
-
-Mail 
-FaceBook
-Instagram
-
-Formulario
-
-*/
