79cec61e6bd9dfabc7da8465bd096fc66399bde0
Ailín <ailin.tobares@gmail.com>
2023-02-13 15:24:07 -0300

    Update 13/02/2023

--- src/assets/Sliders/Who/WhoImages.js
+++ src/assets/Sliders/Who/WhoImages.js
@@ -1,6 +1,6 @@
 
 import ailin from "./Ailin.jpg";
-import marina from "./Marina.jpg";
+/* import marina from "./Marina.jpg"; */
 import matilde from "./Matilde1.png";
 import pedro from "./Pedritus.jpg";
 
@@ -11,7 +11,7 @@ const whoPics = [
     description: "Relevamiento de patologías y avance de obra - Confitería El Molino - Año 2020, Progorod S.A."
   },
   {
-    url: marina, 
+    url: ailin, 
     alt: "Marina A. Pellegrini",
     description: "Retoque pictórico - Retablo de la Parroquia San Ignacio de Loyola - Año 2019"
   },
